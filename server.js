2740a5357ccbf29488fd2bc4724af3b58ca16671
WhitesideX8 <jwhiteside@emsinfo.org>
2026-04-24 17:13:28 -0400

    Refactor server.js for port configuration and formatting
    Updated server.js to use environment variable for port and added newline at end of file.

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ Give feedback:
   }
 });
 
-app.listen(3000, () => {
-  console.log("Go to http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
